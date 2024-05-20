efb18758b682c3d05819878197ce81072b6b85ed
Luiz Jacson <luiz.jacson@souunit.com.br>
2024-05-20 20:54:09 -0300

    feat: adicionando readme

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const app = express();
-const swaggerUi = require('swagger-ui-express')
+const swaggerUi = require('swagger-ui-express');
 const PORT = process.env.PORT || 3000; 
-const conteudoRoutes = require('./routes/conteudoRoutes')
-const instrucaoRoutes = require('./routes/instrucaoRoutes')
-const genericoRoutes = require('./routes/genericoRoutes')
+const conteudoRoutes = require('./routes/conteudoRoutes');
+const instrucaoRoutes = require('./routes/instrucaoRoutes');
+const genericoRoutes = require('./routes/genericoRoutes');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-app.use(express.json())
+const fs = require('fs');
 
-app.use('/inserirInstrucao', instrucaoRoutes)
-app.use('/gerarConteudoEspecifico', conteudoRoutes)
-app.use('/gerarConteudoGenerico', genericoRoutes)
+const path = require('path');
 
-app.use('/api-TinBolt-GEMINI', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Caminho absoluto para o arquivo YAML
+const yamlPath = path.resolve(__dirname, 'swagger.yaml');
+console.log(yamlPath)
+// Carrega o arquivo YAML de forma assíncrona
+fs.readFile(yamlPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao carregar o arquivo YAML:', err);
+        return;
+    }
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    // Parseia o conteúdo do arquivo YAML
+    const swaggerDocument = YAML.parse(data);
+
+    // Configura o Express
+    app.use(express.json());
+    app.use('/inserirInstrucao', instrucaoRoutes);
+    app.use('/gerarConteudoEspecifico', conteudoRoutes);
+    app.use('/gerarConteudoGenerico', genericoRoutes);
+
+    // Adiciona o Swagger UI somente se o arquivo YAML for carregado com sucesso
+    if (swaggerDocument) {
+        app.use('/api-TinBolt-GEMINI', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    }
+
+    // Inicia o servidor
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
 });
